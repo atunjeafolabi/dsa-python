e5c420fb27338efb74891c81156f3894156a7614
Atunje <atunje_afolabi@yahoo.com>
2024-07-16 22:56:10 +0200

    optimized bubble sort

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from BubbleSort import bubble_sort
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    numbers = [3, 4, 8, 10, 1, 2, 1, 20, 15, 7]
+    print(bubble_sort(numbers))
