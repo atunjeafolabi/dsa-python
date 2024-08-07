7d963c96ac861f3a2ef5c11640981d5ae85a81c4
Atunje <atunje_afolabi@yahoo.com>
2024-08-07 16:13:18 +0200

    add another implementation of mergesort

---
+++ MergeSort2.py
@@ -0,0 +1,46 @@
+def mergeSort(array):
+    if len(array) <= 1:
+        return
+
+    midpoint = len(array) // 2
+    left = array[:midpoint]
+    right = array[midpoint:]
+
+    mergeSort(left)
+    mergeSort(right)
+
+    i = j = k = 0
+
+    # merge
+    # Until we reach either end of either left or right,
+    # pick larger among elements left and right and place
+    # them in the correct position at A[p..midpoint]
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+
+    # When we run out of elements in either left or right,
+    # pick up the remaining elements and put in A[p..midpoint]
+    while i < len(left):
+        array[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        array[k] = right[j]
+        j += 1
+        k += 1
+
+
+# Driver program
+if __name__ == '__main__':
+    array = [6, 5, 12, 5, 10, 9, 1]
+
+    mergeSort(array)
+
+    print(array)
