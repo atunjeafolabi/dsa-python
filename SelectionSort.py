171cb1ec5e7660a38aca2d07cdc524dc45f94456
Atunje <atunje_afolabi@yahoo.com>
2024-08-08 21:59:05 +0200

    refactor selection sort and rename binary search file

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,10 +1,17 @@
 def selection_sort(arr):
-    for i in range(0, len(arr) - 1):
+    n = len(arr)
+    for i in range(0, n - 1):
         cur_min_idx = i
-        for j in range(i + 1, len(arr)):
+
+        for j in range(i + 1, n):
             if arr[j] < arr[cur_min_idx]:
                 cur_min_idx = j
-        arr[i], arr[cur_min_idx] = arr[cur_min_idx], arr[i]  # swap
+
+        swap(arr, cur_min_idx, i)
+
+
+def swap(arr, cur_min_idx, i):
+    arr[i], arr[cur_min_idx] = arr[cur_min_idx], arr[i]
 
 
 numbers = [2, 6, 5, 1, 2, 3, 4]
