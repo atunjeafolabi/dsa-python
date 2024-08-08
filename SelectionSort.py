77b897d5cccf451571be24c929fbbf75c46100c4
Atunje <atunje_afolabi@yahoo.com>
2024-08-08 22:04:24 +0200

    remove blank lines

--- SelectionSort.py
+++ SelectionSort.py
@@ -2,11 +2,9 @@ def selection_sort(arr):
     n = len(arr)
     for i in range(0, n - 1):
         cur_min_idx = i
-
         for j in range(i + 1, n):
             if arr[j] < arr[cur_min_idx]:
                 cur_min_idx = j
-
         swap(arr, cur_min_idx, i)
 
 
